7768a886a24973b406cdbd2f88afc18bdbfdddd6
edzen12 <oichiev.edzen@gmail.com>
2026-05-05 19:23:10 +0600

    news view

--- apps/news/models.py
+++ apps/news/models.py
@@ -4,7 +4,7 @@ from django_ckeditor_5.fields import CKEditor5Field
 
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Название')
-    image = models.ImageField(upload_to='news/')
+    image = models.ImageField(upload_to='news/', null=True, blank=True)
     desc = CKEditor5Field('Описание', config_name='extends')
 
     def __str__(self):
--- apps/news/views.py
+++ apps/news/views.py
@@ -1,8 +1,11 @@
-from django.views.generic import TemplateView 
+from django.views.generic import TemplateView, ListView, DetailView
+from apps.news.models import News
 
 
-class NewsView(TemplateView):
+class NewsView(ListView):
+    model = News
     template_name = 'pages/blog.html'
+    context_object_name = 'news'
 
 
 class AboutView(TemplateView):
