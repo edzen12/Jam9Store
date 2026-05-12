88e93529d34cbed97444db7bd5c9e81bbf3b1cf2
edzen12 <oichiev.edzen@gmail.com>
2026-05-12 18:42:38 +0600

    blog single

--- apps/news/admin.py
+++ apps/news/admin.py
@@ -5,6 +5,7 @@ from apps.news.models import News, NewsComment
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'is_active', 'created_at')
+    list_display_links = ('id', 'title',)
     prepopulated_fields = {'slug':('title',)}
 
 
